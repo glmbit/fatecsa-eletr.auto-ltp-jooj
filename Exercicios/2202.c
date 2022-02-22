f37c599434e5ce5b94a3404924199e990b6a98d1
glmbit <99343192+glmbit@users.noreply.github.com>
2022-02-22 12:39:45 -0300

    Add files via upload

---
+++ Exercicios/2202.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define e_numero 45157
+#define s_numero e_numero*10
+
+int main()
+{
+   printf("hello world");
+   printf("\nhello world again");
+   printf("\nAluno: %s \t nota: %f", "Jooj" , 7.69);
+   printf ("\nnumero decimal: %d\t" , s_numero);
+
+    return 0;
+}
