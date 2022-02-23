b7d75859641c322b316bc705900699ba3e6771ac
glmbit <99343192+glmbit@users.noreply.github.com>
2022-02-23 12:42:58 -0300

    Add files via upload

---
+++ Exercicios/Calculo média.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+printf("Digite a nota de P1: ");
+float e1_p1,;
+printf("Digite a nota de P2: ");
+float e2_p2
+printf("Digite a nota de L");
+float el_l
+scanf("%f", & e_metrospsegundo);
+s_kmphora= 3.6* e_metrospsegundo;
+printf("Resultado:");
+printf("%f\n",s_kmphora);
+    return 0;
+}
