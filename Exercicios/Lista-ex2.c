6d63f02c6317d4c8c68d99f3db5e578dcb671ae0
glmbit <99343192+glmbit@users.noreply.github.com>
2022-04-05 12:01:21 -0300

    Add files via upload

---
+++ Exercicios/Lista-ex2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float e_resistor1, e_resistor2, e_resistor3, e_resistor4, s_result;
+    
+    printf("Cálculo resistores em série");
+    printf("\nDigite o valor do 1º resistor:");
+    scanf("%f", & e_resistor1);
+    printf("\nDigite o valor do 2º resistor:");
+    scanf("%f", & e_resistor2);
+    printf("\nDigite o valor do 3º resistor:");
+    scanf("%f", & e_resistor3);
+    printf("\nDigite o valor do 4º resistor:");
+    scanf("%f", & e_resistor4);
+    s_result= 1/(1/e_resistor1+1/e_resistor2+1/e_resistor3+1/e_resistor4);
+    printf("Resultado:");
+    printf("%f\n",s_result);
+    return 0;
+}
